8d0328a72480e2e6b32eb4190788499b8eb584a3
Avisha Dilshan <avishadilshan01@gmail.com>
2026-07-29 11:53:11 +0530

    Initial commit

---
+++ agent.py
@@ -0,0 +1,227 @@
+from web3 import Web3
+from web3.contract import Contract
+from decimal import Decimal, ROUND_HALF_UP
+from dataclasses import dataclass
+from datetime import datetime, timezone, timedelta
+from pprint import pprint
+import time
+import functools
+import sys
+from google import genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print = functools.partial(print, flush=True)
+
+MAINNET_URL = "https://eth-mainnet.g.alchemy.com/v2/alch_6t0cAYZzxaWGlqbogb3M3"
+BLOCK_TIME_SECONDS = 12
+MINUTE_BLOCKS = int(60 / BLOCK_TIME_SECONDS)
+HOUR_BLOCKS = MINUTE_BLOCKS * 60
+DAY_BLOCKS = HOUR_BLOCKS * 24
+CYCLE_BLOCKS = DAY_BLOCKS
+
+# The addresses of the pools we're reading
+WETHUSDC_ADDRESS = Web3.to_checksum_address("0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640")
+WETHWBTC_ADDRESS = Web3.to_checksum_address("0xCBCdF9626bC03E24f779434178A73a0B4bad62eD")
+
+POOL_ABI = [
+    {
+        "inputs": [],
+        "name": "slot0",
+        "outputs": [
+            {"internalType": "uint160", "name": "sqrtPriceX96", "type": "uint160"},
+            {"internalType": "int24", "name": "tick", "type": "int24"},
+            {"internalType": "uint16", "name": "observationIndex", "type": "uint16"},
+            {"internalType": "uint16", "name": "observationCardinality", "type": "uint16"},
+            {"internalType": "uint16", "name": "observationCardinalityNext", "type": "uint16"},
+            {"internalType": "uint8", "name": "feeProtocol", "type": "uint8"},
+            {"internalType": "bool", "name": "unlocked", "type": "bool"},
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "token0",
+        "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "token1",
+        "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+]
+
+ERC20_ABI = [
+    {
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [
+            {"name": "", "type": "string"}
+        ],
+        "payable": False,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": True,
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [
+            {"name": "", "type": "uint8"}
+        ],
+        "payable": False,
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
+
+w3 = Web3(Web3.HTTPProvider(MAINNET_URL))
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+    ) 
+
+@dataclass(frozen=True)
+class ERC20Token:
+    address: str
+    symbol: str
+    decimals: int
+    contract: Contract
+
+    
+@dataclass(frozen=True)
+class PoolInfo:
+    address: str
+    token0: ERC20Token
+    token1: ERC20Token
+    contract: Contract
+    asset: str
+    decimal_factor: Decimal = 1
+    reverse: bool = False
+
+    def get_price(self, block: int) -> Decimal:
+        assert block <= w3.eth.block_number, "Block is in the future"
+        sqrt_price_x96 = Decimal(self.contract.functions.slot0().call(block_identifier=block)[0])
+        raw_price = (sqrt_price_x96 / Decimal(2**96)) ** 2  # (token1 per token0)
+        if self.reverse:
+            return 1/(raw_price * self.decimal_factor)
+        else:
+            return raw_price * self.decimal_factor
+
+
+@dataclass(frozen=True)
+class Quote:
+    timestamp: str
+    price: Decimal
+    asset: str
+
+
+def read_token(address: str) -> ERC20Token:
+    token = w3.eth.contract(address=address, abi=ERC20_ABI)
+    symbol = token.functions.symbol().call()
+    decimals = token.functions.decimals().call()
+
+    return ERC20Token(
+        address=address,
+        symbol=symbol,
+        decimals=decimals,
+        contract=token
+    )
+
+def read_pool(address: str, reverse: bool = False) -> PoolInfo:
+    pool_contract = w3.eth.contract(address=address, abi=POOL_ABI)
+    token0Address = pool_contract.functions.token0().call()
+    token1Address = pool_contract.functions.token1().call()
+    token0 = read_token(token0Address)
+    token1 = read_token(token1Address)
+
+    return PoolInfo(
+        address=address,
+        asset= f"{token1.symbol}/{token0.symbol}" if reverse else f"{token0.symbol}/{token1.symbol}",
+        token0=token0,
+        token1=token1,
+        contract=pool_contract,
+        decimal_factor=Decimal(10) ** Decimal(token0.decimals - token1.decimals),
+        reverse=reverse
+    )
+
+def get_quote(pool: PoolInfo, block_number: int = None) -> Quote:
+    if block_number is None:
+        block_number = w3.eth.block_number
+    block = w3.eth.get_block(block_number)
+    price = pool.get_price(block_number)
+    return Quote(
+        timestamp=datetime.fromtimestamp(block.timestamp, timezone.utc).isoformat(),
+        price=price.quantize(Decimal("0.01")),
+        asset=pool.asset
+    )
+
+def get_quotes(pool: PoolInfo, start_block: int, end_block: int, step: int) -> list[Quote]:
+    quotes = []
+    for block in range(start_block, end_block + 1, step):
+        quote = get_quote(pool, block)
+        quotes.append(quote)
+    return quotes
+
+def format_quotes(quotes: list[Quote]) -> str:
+    result = f"Asset: {quotes[0].asset}\n"
+    for quote in quotes:
+        result += f"\t{quote.timestamp[0:16]} {quote.price.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)}\n"
+    return result
+
+def make_prompt(quotes: list[list[Quote]], expected_time: str, asset: str) -> str:
+    return f"""
+Given these quotes:
+{
+    functools.reduce(lambda acc, q: acc + '\n' + q,
+        map(lambda q: format_quotes(q), quotes))
+}
+
+What would you expect the value for {asset} to be at time {expected_time}?
+
+Provide your answer as a single number rounded to two decimal places,
+without any other text.
+    """
+
+wethusdc_pool = read_pool(WETHUSDC_ADDRESS, True)
+wethusdc_quotes = get_quotes(
+    wethusdc_pool,
+    w3.eth.block_number - 12*CYCLE_BLOCKS,
+    w3.eth.block_number,
+    CYCLE_BLOCKS,
+)
+
+wethwbtc_pool = read_pool(WETHWBTC_ADDRESS)
+wethwbtc_quotes = get_quotes(
+    wethwbtc_pool,
+    w3.eth.block_number - 12*CYCLE_BLOCKS,
+    w3.eth.block_number,
+    CYCLE_BLOCKS
+)
+
+future_time = (datetime.now(timezone.utc) + timedelta(days=1)).isoformat()[0:16]
+prompt = make_prompt([wethusdc_quotes,wethwbtc_quotes], future_time, wethusdc_pool.asset)
+
+response = client.models.generate_content(
+    model="models/gemini-flash-latest",
+    contents=prompt
+)
+
+prediction = response.text.strip()
+print("LLM Response:", prediction)
+expected_price = Decimal(prediction.split()[0])
+current_price = wethusdc_quotes[-1].price
+
+print ("Current price:", wethusdc_quotes[-1].price)
+print(f"In {future_time}, expected price: {expected_price} USD")
+
+if (expected_price > current_price):
+    print(f"Buy, I expect the price to go up by {expected_price - current_price} USD")
+else:
+    print(f"Sell, I expect the price to go down by {current_price - expected_price} USD")   
---
+++ test-predictor.py
@@ -0,0 +1,266 @@
+import re
+from web3 import Web3
+from web3.contract import Contract
+from decimal import Decimal, ROUND_HALF_UP
+from dataclasses import dataclass
+from datetime import datetime, timezone, timedelta
+from pprint import pprint
+import time
+import functools
+import sys
+from google import genai
+from google.genai.errors import ServerError
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print = functools.partial(print, flush=True)
+
+MAINNET_URL = "https://eth-mainnet.g.alchemy.com/v2/alch_6t0cAYZzxaWGlqbogb3M3"
+BLOCK_TIME_SECONDS = 12
+MINUTE_BLOCKS = int(60 / BLOCK_TIME_SECONDS)
+HOUR_BLOCKS = MINUTE_BLOCKS * 60
+DAY_BLOCKS = HOUR_BLOCKS * 24
+CYCLE_BLOCKS = DAY_BLOCKS
+CYCLES_BACK = 12 # How many cycles we look back
+CYCLES_FOR_TEST = 40 # For the backtest, how many cycles we test over
+
+# The addresses of the pools we're reading
+WETHUSDC_ADDRESS = Web3.to_checksum_address("0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640")
+WETHWBTC_ADDRESS = Web3.to_checksum_address("0xCBCdF9626bC03E24f779434178A73a0B4bad62eD")
+
+POOL_ABI = [
+    {
+        "inputs": [],
+        "name": "slot0",
+        "outputs": [
+            {"internalType": "uint160", "name": "sqrtPriceX96", "type": "uint160"},
+            {"internalType": "int24", "name": "tick", "type": "int24"},
+            {"internalType": "uint16", "name": "observationIndex", "type": "uint16"},
+            {"internalType": "uint16", "name": "observationCardinality", "type": "uint16"},
+            {"internalType": "uint16", "name": "observationCardinalityNext", "type": "uint16"},
+            {"internalType": "uint8", "name": "feeProtocol", "type": "uint8"},
+            {"internalType": "bool", "name": "unlocked", "type": "bool"},
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "token0",
+        "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "token1",
+        "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+]
+
+ERC20_ABI = [
+    {
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [
+            {"name": "", "type": "string"}
+        ],
+        "payable": False,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": True,
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [
+            {"name": "", "type": "uint8"}
+        ],
+        "payable": False,
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
+
+w3 = Web3(Web3.HTTPProvider(MAINNET_URL))
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
+@dataclass(frozen=True)
+class ERC20Token:
+    address: str
+    symbol: str
+    decimals: int
+    contract: Contract
+
+    
+@dataclass(frozen=True)
+class PoolInfo:
+    address: str
+    token0: ERC20Token
+    token1: ERC20Token
+    contract: Contract
+    asset: str
+    decimal_factor: Decimal = 1
+    reverse: bool = False
+
+    def get_price(self, block: int) -> Decimal:
+        assert block <= w3.eth.block_number, "Block is in the future"
+        sqrt_price_x96 = Decimal(self.contract.functions.slot0().call(block_identifier=block)[0])
+        raw_price = (sqrt_price_x96 / Decimal(2**96)) ** 2  # (token1 per token0)
+        if self.reverse:
+            return 1/(raw_price * self.decimal_factor)
+        else:
+            return raw_price * self.decimal_factor
+
+
+@dataclass(frozen=True)
+class Quote:
+    timestamp: str
+    price: Decimal
+    asset: str
+
+
+def read_token(address: str) -> ERC20Token:
+    token = w3.eth.contract(address=address, abi=ERC20_ABI)
+    symbol = token.functions.symbol().call()
+    decimals = token.functions.decimals().call()
+
+    return ERC20Token(
+        address=address,
+        symbol=symbol,
+        decimals=decimals,
+        contract=token
+    )
+
+def read_pool(address: str, reverse: bool = False) -> PoolInfo:
+    pool_contract = w3.eth.contract(address=address, abi=POOL_ABI)
+    token0Address = pool_contract.functions.token0().call()
+    token1Address = pool_contract.functions.token1().call()
+    token0 = read_token(token0Address)
+    token1 = read_token(token1Address)
+
+    return PoolInfo(
+        address=address,
+        asset= f"{token1.symbol}/{token0.symbol}" if reverse else f"{token0.symbol}/{token1.symbol}",
+        token0=token0,
+        token1=token1,
+        contract=pool_contract,
+        decimal_factor=Decimal(10) ** Decimal(token0.decimals - token1.decimals),
+        reverse=reverse
+    )
+
+def get_quote(pool: PoolInfo, block_number: int = None) -> Quote:
+    if block_number is None:
+        block_number = w3.eth.block_number
+    block = w3.eth.get_block(block_number)
+    price = pool.get_price(block_number)
+    return Quote(
+        timestamp=datetime.fromtimestamp(block.timestamp, timezone.utc).isoformat(),
+        price=price.quantize(Decimal("0.01")),
+        asset=pool.asset
+    )
+
+def get_quotes(pool: PoolInfo, start_block: int, end_block: int, step: int) -> list[Quote]:
+    quotes = []
+    for block in range(start_block, end_block + 1, step):
+        quote = get_quote(pool, block)
+        quotes.append(quote)
+    return quotes
+
+def format_quotes(quotes: list[Quote]) -> str:
+    result = f"Asset: {quotes[0].asset}\n"
+    for quote in quotes:
+        result += f"\t{quote.timestamp[0:16]} {quote.price.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)}\n"
+    return result
+
+def make_prompt(quotes: list[list[Quote]], expected_time: str, asset: str) -> str:
+    return f"""
+Given these quotes:
+{
+    functools.reduce(lambda acc, q: acc + '\n' + q,
+        map(lambda q: format_quotes(q), quotes))
+}
+
+What would you expect the value for {asset} to be at time {expected_time}?
+
+Provide your answer as a single number rounded to two decimal places,
+without any other text.
+    """
+
+
+# Get lots of quotes
+wethusdc_pool = read_pool(WETHUSDC_ADDRESS, True)
+wethusdc_quotes = get_quotes(
+    wethusdc_pool,
+    w3.eth.block_number - CYCLE_BLOCKS*CYCLES_FOR_TEST,
+    w3.eth.block_number,
+    CYCLE_BLOCKS,
+)
+
+wethwbtc_pool = read_pool(WETHWBTC_ADDRESS)
+wethwbtc_quotes = get_quotes(
+    wethwbtc_pool,
+    w3.eth.block_number - CYCLE_BLOCKS*CYCLES_FOR_TEST,
+    w3.eth.block_number,
+    CYCLE_BLOCKS
+)
+
+# Create predictions and check them against real history
+
+total_error = Decimal(0)
+changes = []
+
+for index in range(0,len(wethusdc_quotes)-CYCLES_BACK):
+    wethusdc_slice = wethusdc_quotes[index:index+CYCLES_BACK]
+    wethwbtc_slice = wethwbtc_quotes[index:index+CYCLES_BACK]
+    prediction_time = wethusdc_quotes[index+CYCLES_BACK].timestamp[0:16]
+    prompt = make_prompt([wethusdc_slice,wethwbtc_slice], prediction_time, wethusdc_pool.asset)
+    try:
+        while True:
+            try:
+                response = client.models.generate_content(
+                    model="models/gemini-flash-latest",
+                    contents=prompt
+                )
+                break
+
+            except ServerError:
+                print("Gemini busy... waiting 10 seconds...")
+                time.sleep(10)
+
+    except KeyboardInterrupt:
+        print("\nProgram stopped by user.")
+        exit()
+
+    prediction = response.text.strip()
+
+    predicted_price = Decimal(prediction.split()[0])
+
+    real_price = wethusdc_quotes[index + CYCLES_BACK].price
+    prediction_time_price = wethusdc_quotes[index + CYCLES_BACK - 1].price
+
+    error = abs(predicted_price - real_price)
+    total_error += error
+    print (f"Prediction for {prediction_time}: predicted {predicted_price} USD, real {real_price} USD, error {error} USD")
+
+    recomended_action = 'buy' if predicted_price > prediction_time_price else 'sell'
+    price_increase = real_price - prediction_time_price
+    changes.append(price_increase if recomended_action == 'buy' else -price_increase)
+
+
+print (f"Mean prediction error over {len(wethusdc_quotes)-CYCLES_BACK} predictions: {total_error / Decimal(len(wethusdc_quotes)-CYCLES_BACK)} USD")
+
+length_changes = Decimal(len(changes))
+mean_change = sum(changes, Decimal(0)) / length_changes
+print (f"Mean change per recommendation: {mean_change} USD")
+var = sum((x - mean_change) ** 2 for x in changes) / length_changes
+print (f"Standard variance of changes: {var.sqrt().quantize(Decimal("0.01"))} USD")
+print (f"Profitable days: {len(list(filter(lambda x: x > 0, changes)))/length_changes:.2%}")
+print (f"Losing days: {len(list(filter(lambda x: x < 0, changes)))/length_changes:.2%}")
